00be2eedabbcb39345dd2964d90be29cf0746e2c
douglaslim <douglaslim95@gmail.com>
2022-05-01 15:54:26 +0800

    feat: Add tailwind css and complete components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Projects from './components/Projects'
@@ -9,7 +8,7 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+];
+
+export const skills = [
+  "Golang",
+  "Java",
+  "Spring Boot",
+  "Spring Batch",
+  "Vue.js",
+  "Angular 8",
+  "Microsoft Azure",
+  "Git",
+  "MySQL",
+  "Mongodb",
+  "Hibernate",
+  "Jest",
+  "Cypress",
+  "Kafka",
+  "Microservice"
+];
+
+export const testimonials = [
+  {
+    quote: "",
+    image: "",
+    name: "Ipsum",
+    company: "Test",
+  },
+];
